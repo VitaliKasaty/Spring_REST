0a69228ee4fa297600f15de1c0dccc67237d0c87
Vitali Kasaty <vitalik007007@gmail.com>
2022-06-16 20:12:34 +0300

    Delete employee with DeleteMapping method

--- src/main/java/com/kasatyvitali/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/kasatyvitali/spring/rest/controller/MyRESTController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -58,8 +59,20 @@ public class MyRESTController {
 		
 		employeeService.saveEmployee(employee);
 		
-		return employee;
+		return employee;		
+	}
+	
+	@DeleteMapping("/employees/{id}")
+	public String deleteEmployee(@PathVariable int id) {
 		
+		Employee employee = employeeService.getEmployee(id);
+		if (employee == null) {
+			throw new NoSuchEmployeeException("There is no Employee with "
+					+ "id = " + id + " in Database");
+		}
+		employeeService.deleteEmployee(id);
+		
+		return "Employee with ID = " + id + " was deleted.";
 	}
 
 }
