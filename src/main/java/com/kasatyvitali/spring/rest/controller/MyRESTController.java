0111aca0305f738e645330ac9838f98090dd974a
Vitali Kasaty <vitalik007007@gmail.com>
2022-06-16 19:51:59 +0300

    Add function: save employee with POST controller method

--- src/main/java/com/kasatyvitali/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/kasatyvitali/spring/rest/controller/MyRESTController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,6 +42,14 @@ public class MyRESTController {
 					+ id + " in DataBase");
 		} 
 		return employee;		
+	}
+	
+	@PostMapping("/employees")
+	public Employee addNewEmployee(@RequestBody Employee employee) {
+		
+		employeeService.saveEmployee(employee);
+		
+		return employee;
 	}
 
 }
