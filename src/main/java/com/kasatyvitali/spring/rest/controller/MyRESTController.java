bd507b2642010b36b93ee7d8e8222a642e299260
Vitali Kasaty <vitalik007007@gmail.com>
2022-06-16 21:59:00 +0300

    Some clean fix

--- src/main/java/com/kasatyvitali/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/kasatyvitali/spring/rest/controller/MyRESTController.java
@@ -3,10 +3,7 @@ package com.kasatyvitali.spring.rest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kasatyvitali.spring.rest.entity.Employee;
-import com.kasatyvitali.spring.rest.exception_handling.EmployeeIncorrectData;
 import com.kasatyvitali.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.kasatyvitali.spring.rest.service.EmployeeService;
 
